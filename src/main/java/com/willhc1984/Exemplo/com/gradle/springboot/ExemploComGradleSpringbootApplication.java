93c3bbb5812766aa6a3cfe41fb9eaba864ea6e3c
unknown <willhc1984@gmail.com>
2020-05-28 23:38:43 -0300

    Setup gradle-springboot

---
+++ src/main/java/com/willhc1984/Exemplo/com/gradle/springboot/ExemploComGradleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.willhc1984.Exemplo.com.gradle.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploComGradleSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploComGradleSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willhc1984/Exemplo/com/gradle/springboot/ExemploComGradleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willhc1984.Exemplo.com.gradle.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploComGradleSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
